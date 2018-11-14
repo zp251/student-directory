effba05543e428c435faed8ff729a88489c2620e
zoe pot <pot.zoe.s@gmail.com>
2018-11-14 16:42:41 +0000

    asking for user input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,22 @@
-# let's put all the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# and then some methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -27,6 +31,8 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+students = input_students
 # finally, we call the methods
 print_header
 print(students)

99ba2db4aa142ef90414ec6e408dffa3096cc5ec
zoe pot <pot.zoe.s@gmail.com>
2018-11-16 12:03:13 +0000

    request additional student info: nationality and favourite sport (Exercise 5)

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the student"
+  puts "To finish, just hit return three times"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  puts "Please enter the nationality of the student"
+  nationality = gets.chomp
+  puts "Please enter the favourite sport of the student"
+  sport = gets.chomp
+  
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, nationality: nationality, sport: sport }
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Let's enter a new student: name?"
     name = gets.chomp
+    if name == "" 
+      break
+    end
+    puts "nationality?"
+    nationality = gets.chomp
+    puts "sport?"
+    sport = gets.chomp
   end
   # return the array of students
   students
@@ -26,7 +39,7 @@ def print(students)
   student_count = 0
   until student_count == students.count
     students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort), nationality: #{student[:nationality]}, favourite sport: #{student[:sport]}"
         if student_count == students.count
           break
         end

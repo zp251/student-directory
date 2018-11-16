9866f8f2ec93df36ed114ff97c8f526587c53b2f
zoe pot <pot.zoe.s@gmail.com>
2018-11-16 10:58:25 +0000

    Print students whose name begins with a z (Exercise 2)

--- directory.rb
+++ directory.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
+    if student[:name][0] == "z"
   puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 

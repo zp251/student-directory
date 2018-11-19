b1bf1b5fb5f8c24a177583700d833c6d3f444407
zoe pot <pot.zoe.s@gmail.com>
2018-11-19 16:18:08 +0000

    Added option to load CSV file (Step 12)

--- directory.rb
+++ directory.rb
@@ -25,6 +25,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -42,6 +43,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -72,6 +75,15 @@ def save_students
     csv_line = student_data.join(",")
     file.puts csv_line
   end
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu

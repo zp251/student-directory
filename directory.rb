4a1e5469699a25d5f48687b990c9c3a836e1110d
zoe pot <pot.zoe.s@gmail.com>
2018-11-19 15:42:50 +0000

    Addition of .gitignore file

--- directory.rb
+++ directory.rb
@@ -24,6 +24,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -39,6 +40,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -60,6 +63,15 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name]], [student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
 end
 
 interactive_menu

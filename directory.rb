c45b8cf22d8a07784d4d982cc57bec64647071c7
zoe pot <pot.zoe.s@gmail.com>
2018-11-20 11:37:20 +0000

    feedback messages added (Exercise 4)

--- directory.rb
+++ directory.rb
@@ -17,11 +17,20 @@ end
 
 def process(selection)
   case selection
-  when "1" then input_students
-  when "2" then show_students
-  when "3" then save_students
-  when "4" then load_students
-  when "9"then exit
+  when "1"
+    puts "You have chosen Option 1. 'Input the students'"
+    input_students
+  when "2"
+    puts "You have chosen Option 2. Here is the list of students "
+    show_students
+  when "3"
+    puts "You have chosen Option 3. List saved successfully"
+    save_students
+  when "4"
+    puts "You have chosen Option 4. List loaded successfully"
+    load_students
+  when "9"
+    exit
   else
     puts "I don't know that you meant, try again!"
   end

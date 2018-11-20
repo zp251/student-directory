8efecf893c4b3bb8792e853ebb982430bc98d5ff
zoe pot <pot.zoe.s@gmail.com>
2018-11-20 11:27:06 +0000

    Refractor further (Exercise 3)

--- directory.rb
+++ directory.rb
@@ -17,16 +17,11 @@ end
 
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
+  when "1" then input_students
+  when "2" then show_students
+  when "3" then save_students
+  when "4" then load_students
+  when "9"then exit
   else
     puts "I don't know that you meant, try again!"
   end
@@ -91,11 +86,7 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  if ARGV != nil
-    filename = ARGV.first
-  else
-    filename = "students.csv"
-  end
+  ARGV != nil ? filename = ARGV.first : filename = "students.csv"
 
   if filename.nil?
     load_students
